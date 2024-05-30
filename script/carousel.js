769f857238fa5255cff55cede2aff3781a40bd94
ShMPt <cacahouettetbs@gmail.com>
2024-05-30 11:06:03 +0200

    fix: backward loop of prev button + refacto modulos

--- script/carousel.js
+++ script/carousel.js
@@ -4,18 +4,28 @@ const content = document.querySelector('#content');
 let imageData = [
     "green", "aqua", "red", "blue",
 ];
+let imageTitle = [
+    "1" , "2", "3", "4",
+];
 
 content.style.backgroundColor = imageData[0];
 
 
 function nextImage() {
     let index = imageData.indexOf(content.style.backgroundColor);
-    content.style.backgroundColor = imageData[++index % imageData.length];
+    index = (index + 1)%imageData.length
+    content.style.backgroundColor = imageData[index];
+    content.innerText = "Image " + imageTitle[index % imageData.length];
 }
 
 function prevImage() {
     let index = imageData.indexOf(content.style.backgroundColor);
-    content.style.backgroundColor = imageData[Math.abs(--index) % imageData.length];
+
+    if (index - 1 < 0) {index += imageData.length};
+    index = (index - 1) % imageData.length;
+
+    content.style.backgroundColor = imageData[index];
+    content.innerText = "Image " + imageTitle[index];
 }
 
 
