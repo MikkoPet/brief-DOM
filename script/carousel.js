4aeca4e6a87bf6e753111288aa7ccb65706cbd19
ShMPt <cacahouettetbs@gmail.com>
2024-05-30 15:55:15 +0200

    refacto: nest lists for cleaner code

--- script/carousel.js
+++ script/carousel.js
@@ -3,30 +3,33 @@ const content = document.querySelector('#content');
 const caption = document.querySelector('p');
 
 let imageData = [
-    "green", "aqua", "red", "blue",
-];
-let imageTitle = [
-    "1" , "2", "3", "4",
+    ["green", "1"], ["aqua", "2"], ["red", "3"], ["blue", "4"],
 ];
 
-content.style.backgroundColor = imageData[0];
+content.style.backgroundColor = imageData[0][0];
 
 
 function nextImage() {
-    let index = imageData.indexOf(content.style.backgroundColor);
+    let index = currentFinder();
     index = (index + 1)%imageData.length
-    content.style.backgroundColor = imageData[index];
-    caption.innerText = "Image " + imageTitle[index % imageData.length];
+    content.style.backgroundColor = imageData[index][0];
+    caption.innerText = imageData[index][1];
 }
 
 function prevImage() {
-    let index = imageData.indexOf(content.style.backgroundColor);
+    let index = currentFinder();
 
     if (index - 1 < 0) {index += imageData.length};
     index = (index - 1) % imageData.length;
 
-    content.style.backgroundColor = imageData[index];
-    caption.innerText = "Image " + imageTitle[index];
+    content.style.backgroundColor = imageData[index][0];
+    caption.innerText = imageData[index][1];
 }
 
-
+function currentFinder() {
+    for(var i = 0; i < imageData.length; i++) {
+        if(imageData[i][0] === content.style.backgroundColor) {
+          return i;
+        }
+     }
+}
