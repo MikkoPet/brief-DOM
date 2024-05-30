3b9a0241418e3b139060b2fd73fe48355ede513a
ShMPt <cacahouettetbs@gmail.com>
2024-05-30 16:16:11 +0200

    refacto: remove deprecated background colour function and adjust detection function to look for class name

--- script/carousel.js
+++ script/carousel.js
@@ -3,34 +3,31 @@ const content = document.querySelector('#content');
 const caption = document.querySelector('p');
 
 let imageData = [
-    ["green", "Hazy Moon", "image1"], ["aqua", "Rain", "image2"], ["red", "Midnight", "image3"], ["blue", "Cloudy", "image4"],
+    ["Hazy Moon", "image1"], ["Rain", "image2"], ["Midnight", "image3"], ["Cloudy", "image4"],
 ];
 
-content.style.backgroundColor = imageData[0][0];
 
 
 function nextImage() {
     let index = currentFinder();
     index = (index + 1)%imageData.length
-    content.style.backgroundColor = imageData[index][0];
+
     caption.innerText = imageData[index][1];
-    content.classList.replace(imageData[(index + 3)%4][2], imageData[index][2]);
+    content.classList.replace(imageData[(index + 3)%4][1], imageData[index][1]);
 }
 
 function prevImage() {
     let index = currentFinder();
-
     if (index - 1 < 0) {index += imageData.length};
     index = (index - 1) % imageData.length;
 
-    content.style.backgroundColor = imageData[index][0];
     caption.innerText = imageData[index][1];
-    content.classList.replace(imageData[(index + 5)%4][2], imageData[index][2]);
+    content.classList.replace(imageData[(index + 5)%4][1], imageData[index][1]);
 }
 
 function currentFinder() {
-    for(var i = 0; i < imageData.length; i++) {
-        if(imageData[i][0] === content.style.backgroundColor) {
+    for(let i = 0; i < imageData.length; i++) {
+        if(imageData[i][1] === content.className) {
           return i;
         }
      }
