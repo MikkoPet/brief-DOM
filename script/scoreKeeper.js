5c68e20693763ff2175cefc3946583352aa9e929
ShMPt <cacahouettetbs@gmail.com>
2024-05-30 11:53:30 +0200

    feat: (wip) block input of new points once the max score is reached

--- script/scoreKeeper.js
+++ script/scoreKeeper.js
@@ -1,24 +1,31 @@
 let scoreDisplay = document.querySelector('h1');
 let limitDisplay = document.querySelector('p');
 
+let scoreLimit = 1;
+let scorePlayer1 = 0;
+let scorePlayer2 = 0;
+
 function changeLimit() {
-    let newLimit = document.querySelector('input').value;
-    limitDisplay.innerText = `Playing to: ${newLimit}`;
+    scoreLimit = document.querySelector('input').value;
+    limitDisplay.innerText = `Playing to: ${scoreLimit}`;
 
     if (scorePlayer1 !== 0 || scorePlayer2 !== 0) {
         resetPoints();
     }
 }
 
-let scorePlayer1 = 0;
-let scorePlayer2 = 0;
 
 function scoreUpPlayer1 () {
+    if (scorePlayer1 !== scoreLimit || scorePlayer2 !== scoreLimit) {
     scoreDisplay.innerText = `${++scorePlayer1} to ${scorePlayer2}`;
+    }
 }
 function scoreUpPlayer2 () {
+    if (scorePlayer1 !== scoreLimit || scorePlayer2 !== scoreLimit) {
     scoreDisplay.innerText = `${scorePlayer1} to ${++scorePlayer2}`;
+    }
 }
+
 
 function resetPoints() {
     scorePlayer1 = 0;
