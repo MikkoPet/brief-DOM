dd1d4be43689677dd207ddb2ee3b1940238489dd
ShMPt <cacahouettetbs@gmail.com>
2024-05-30 11:36:56 +0200

    feat: added point counter function to react to button presses

--- script/scoreKeeper.js
+++ script/scoreKeeper.js
@@ -1,7 +1,17 @@
-let scoreDisplay = document.querySelector('h1').innerText;
-let limitDisplay = document.querySelector('p').innerText;
+let scoreDisplay = document.querySelector('h1');
+let limitDisplay = document.querySelector('p');
 
 function changeLimit() {
     let newLimit = document.querySelector('input').value;
-    document.querySelector('p').innerText = `Playing to: ${newLimit}`;
+    limitDisplay.innerText = `Playing to: ${newLimit}`;
+}
+
+let scorePlayer1 = 0;
+let scorePlayer2 = 0;
+
+function scoreUpPlayer1 () {
+    scoreDisplay.innerText = `${++scorePlayer1} to ${scorePlayer2}`;
+}
+function scoreUpPlayer2 () {
+    scoreDisplay.innerText = `${scorePlayer1} to ${++scorePlayer2}`;
 }
