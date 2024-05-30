e01dc67c78de71d2cd1719ba6e02a7a6e746bed5
ShMPt <cacahouettetbs@gmail.com>
2024-05-30 10:24:45 +0200

    feat: add function to append input item onto the shopping list

--- script/toDoList.js
+++ script/toDoList.js
@@ -1,3 +1,24 @@
+let shoppingList = document.body.querySelector('ul');
+
+const buttonClick = document.querySelector('button');
+buttonClick.addEventListener("click", addNewItem())
+
+
 function getShoppingListItem() {
+
 const shopItemValue=document.querySelector('input').value;
+return shopItemValue.trim();
+
 };
+
+function addNewItem() {
+
+    let itemContent = getShoppingListItem();
+
+    if (itemContent === "") {return;}
+
+    let newItem = document.createElement('li');
+    newItem.innerText = itemContent;
+    shoppingList.append(newItem);
+
+}
