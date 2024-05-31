9b9a5aa35d15d99392a281f8a66871d39d6cac51
ShMPt <cacahouettetbs@gmail.com>
2024-05-31 10:17:37 +0200

    feat: add delete button (WIP. TBA: make them delete the RIGHT element)

--- script/toDoList.js
+++ script/toDoList.js
@@ -12,20 +12,41 @@ function getShoppingListItem() {
 
 };
 
-function addNewItem() {
 
-    let itemContent = getShoppingListItem();
-    if (itemContent === "") { return; }
-    itemData.push(itemContent);
-
+function refreshList() {
     shoppingList.innerHTML = "";
 
     for (let n in itemData) {
         let newItem = document.createElement('li');
         newItem.innerText = itemData[n];
+
+        let deleteButton = document.createElement('button');
+        deleteButton.setAttribute("type", "button");
+        deleteButton.addEventListener("click", function(){
+            let itemToDelete = this.closest("li");
+            index = itemData.indexOf(itemToDelete);
+            itemData.splice(index, 1);
+            refreshList();
+        })
+        deleteButton.classList.add("deleter");
+        deleteButton.innerText = "x";
+ 
+        newItem.append(deleteButton);
         shoppingList.append(newItem);
     }
+}
+
+function addNewItem() {
+
+    let itemContent = getShoppingListItem();
+    if (itemContent === "") { return; }
+
+    itemData.push(itemContent);
+
+    refreshList();
 
     document.querySelector('form').reset();
 
 }
+
+
